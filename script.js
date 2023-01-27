66ccbc1b885f5f055236d0f039f693f6ea55337d
Kale Kamara <102758341+kale-Dev1@users.noreply.github.com>
2023-01-27 17:34:24 +0000

    remove axios and use fetch

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-import axios from "https://cdn.skypack.dev/axios@1.2.5";
+// import axios from "https://cdn.skypack.dev/axios@1.2.5";
 const laughEmoji = document.getElementById('laughImg')
 laughEmoji.src = "laugh.svg";
-function getJoke(){
-    const config = {
-        headers: {
-            Accept: 'application/json'
-        }
-    }
-    axios.get('https://icanhazdadjoke.com/', config).then(res =>{
-        document.getElementById('joke').innerHTML = res.data.joke
-    })
-}
+// function getJoke(){
+//     const config = {
+//         headers: {
+//             Accept: 'application/json'
+//         }
+//     }
+//     axios.get('https://icanhazdadjoke.com/', config).then(res =>{
+//         document.getElementById('joke').innerHTML = res.data.joke
+//     })
+// }
 
 const jokeBtn = document.getElementById('jokeBtn');
-jokeBtn.addEventListener('click', getJoke);
+jokeBtn.addEventListener('click', handleClick);
+
+async function fetchJoke() {
+    const response = await fetch("https://icanhazdadjoke.com", {
+        headers: {
+            Accept: "application/json",    
+        },
+    })
+
+    return response.json();
+    
+  }
+  async function handleClick() {
+    const { joke } = await fetchJoke();
+    console.log(joke);
+  }
