9f01b4d154bee1a9b14ad82a79c4fafd922be274
Kale Kamara <102758341+kale-Dev1@users.noreply.github.com>
2023-01-27 04:53:20 +0000

    add laughEmoji source

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import axios from "https://cdn.skypack.dev/axios@1.2.5";
 const laughEmoji = document.getElementById('laughImg')
-laughEmoji.src = "http://c1.thejournal.ie/media/2015/10/face-with-tears-of-joy-752x501.png";
+laughEmoji.src = "laugh.svg";
 function getJoke(){
     const config = {
         headers: {
